4084a1553d598c6d46f30cadbd7066be9a53697f
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-09-18 21:22:03 +0530

    Add files via upload

---
+++ Arrays/Reverse.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void reversenum(int arr[]){
+        int first = 0, last = arr.length-1;
+        //Swap
+        while(first < last){
+            int temp =0;
+            temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+
+            first ++;
+            last --;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[6];
+        System.out.println("Enter the Array...");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Reversed Array..");
+        reversenum(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
