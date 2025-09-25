f10d2c4f7da1ca27806eb925708b7a409f3fa9e6
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-09-26 01:42:35 +0530

    Add files via upload

---
+++ Arrays/Question2.java
@@ -0,0 +1,38 @@
+//Program to check if an array is sorted or not
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class Question2 {
+//    public static int sorted(int num[]){
+//        for(int i=1; i< num.length; i++){
+//            if(num[i] < num[i-1]){
+//                return -1;
+//            }
+//        }
+//        return 1;
+//    }
+    //More Optimized
+    public static boolean issorted(int num[]){
+        boolean check = true;
+        for(int i = 1; i< num.length; i++){
+            if(num[i] < num[i-1]){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+    public static void main(String[] args) {
+        int num[] = {2, 11, 8, 11, 18};
+
+//        int x = sorted(num);
+//        if(x == -1){
+//            System.out.println("The array is UnSorted");
+//        }
+//        else
+//            System.out.println("The Array is Sorted");
+        System.out.println("Is Sorted...."+issorted(num));
+    }
+}
