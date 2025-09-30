cd830e907ba5a838564ac630d719f43e5b5f3a28
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-01 00:44:18 +0530

    Add files via upload

---
+++ Arrays/Reverse2.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Reverse2 {
+    static int[] reverseArray(int[] arr){
+        int len = arr.length;
+        int[] ans = new int[len];
+        int j = 0;
+        for(int i=len-1; i>=0; i--){
+            ans[j++] = arr[i];
+        }
+
+        return ans;
+    }
+    static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,11,22,5};
+        int[] ans = reverseArray(arr);
+        printArray(ans);
+    }
+}
