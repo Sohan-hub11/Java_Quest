8e367271a23d7c5cde0b2eec7e8e8c89196ca7a2
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-09 15:57:30 +0530

    Add files via upload

---
+++ 2D Array/Search.java
@@ -0,0 +1,37 @@
+//Normal Search Algorithm..
+
+package Array_2D;
+import java.util.*;
+public class Search {
+    static void search(int[][] arr, int key){
+        for(int i=0; i< arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j] == key){
+                    System.out.println("Element found in : (" + i + ", " + j + ") matrices.");
+                }
+                else
+                    System.out.println("Not Found..");
+            }
+        }
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        System.out.println("Initializing the 2D Array..");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter the Element to Search : ");
+        int k = sc.nextInt();
+
+        search(matrix, k);
+
+    }
+}
