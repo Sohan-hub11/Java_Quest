de60a4377e40071825e022adfdefddb58c7979b3
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-09 13:00:08 +0530

    Add files via upload

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,34 @@
+//Insertion Sort.
+
+package Sorting;
+
+public class InsertionSort {
+    static void insertion_sort(int[] arr){
+        for(int i=1; i< arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            //Finding the position to Insert.
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //Insertion.
+            arr[prev+1] = curr;
+        }
+    }
+    static void printArray(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] arr= {5, 4, 1, 3, 2};
+        System.out.println("Before Sorting..");
+        printArray(arr);
+
+        System.out.println("After Sorting..");
+        insertion_sort(arr); //Sorting
+        printArray(arr);
+    }
+}
