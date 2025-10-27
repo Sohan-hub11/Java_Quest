219135c7d7b97b73d2059832513a0628755c39e9
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-27 16:37:02 +0530

    Add files via upload

---
+++ Linked List/AddFirst.java
@@ -0,0 +1,53 @@
+//Add First in Linked list.
+
+package LinkedList;
+
+public class AddFirst {
+    public static class Node { //Declaration.
+        int data;
+        Node next;
+        public Node(int data){
+            this.data =data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //1. Create a new node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //2. new node's next = head
+        newNode.next = head;
+
+        //3. head = new node
+        head = newNode;
+    }
+    public void printLL() {
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args){
+        AddFirst ll = new AddFirst();
+
+        ll.printLL();
+        ll.addFirst(4);
+        ll.printLL();
+        ll.addFirst(3);
+        ll.printLL();
+        ll.addFirst(2);
+        ll.printLL();
+        ll.addFirst(1);
+        ll.printLL();
+    }
+}
