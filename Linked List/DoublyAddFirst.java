f47e88ed3be4c1e8f212db5e58ade7c4297032be
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-11-01 22:23:23 +0530

    Update DoublyAddFirst.java

--- Linked List/DoublyAddFirst.java
+++ Linked List/DoublyAddFirst.java
@@ -1,4 +1,4 @@
-//Add data at First in Linked list.
+//Add data at First in Doubly Linked list.
 
 package LinkedList;
 
@@ -6,17 +6,21 @@ public class DoublyAddFirst {
     public static class Node { //Declaration.
         int data;
         Node next;
+        Node prev;
         public Node(int data){
             this.data =data;
             this.next = null;
+            this.prev = null;
         }
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){
         //1. Create a new node
         Node newNode = new Node(data);
+        size++;
 
         if(head == null){
             head = tail = newNode;
@@ -25,29 +29,30 @@ public class DoublyAddFirst {
 
         //2. new node's next = head
         newNode.next = head;
+        head.prev = null;
 
         //3. head = new node
         head = newNode;
     }
+
     public void printLL() {
         Node temp = head;
         while(temp != null){
-            System.out.print(temp.data + "->");
+            System.out.print(temp.data + "<->");
             temp = temp.next;
         }
         System.out.println("null");
     }
     public static void main(String[] args){
-        DoublyAddFirst ll = new DoublyAddFirst();
+        DoublyAddFirst dll = new DoublyAddFirst();
 
-        ll.printLL();
-        ll.addFirst(4);
-        ll.printLL();
-        ll.addFirst(3);
-        ll.printLL();
-        ll.addFirst(2);
-        ll.printLL();
-        ll.addFirst(1);
-        ll.printLL();
+        dll.addFirst(5);
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.printLL();
+        System.out.println(size);
     }
 }
