c7f1884efa0143b3499a5ae9669d20032a6186ec
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-11-08 22:12:59 +0530

    Add files via upload

---
+++ MixedQuestions/Q2.java
@@ -0,0 +1,12 @@
+package MixedQuestions;
+import java.util.*;
+public class Q2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            E[i] = sc.nextInt();
+        }
+
+    }
+}
