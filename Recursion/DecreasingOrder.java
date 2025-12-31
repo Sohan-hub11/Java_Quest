901ac0f7eeb71e3e3b3a4d142b06b93b72c70b32
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-12-31 15:59:34 +0530

    Add files via upload

---
+++ Recursion/DecreasingOrder.java
@@ -0,0 +1,18 @@
+//Print Number in Decreasing Order.
+
+package Recursion;
+
+public class DecreasingOrder {
+    public static void printDec(int n){
+        if(n == 1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n+ " ");
+        printDec(n-1);
+    }
+    public static void main(String[] args){
+        int n = 10;
+        printDec(n);
+    }
+}
