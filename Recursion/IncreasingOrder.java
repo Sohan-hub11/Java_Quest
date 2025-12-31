9a6fff26018fe206690bf2ed45a577d71aa6b08e
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-12-31 15:59:53 +0530

    Add files via upload

---
+++ Recursion/IncreasingOrder.java
@@ -0,0 +1,19 @@
+//Print Number in Increasing Order.
+
+package Recursion;
+
+public class IncreasingOrder {
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(n+ " ");
+            return;
+        }
+
+        printInc(n-1);
+        System.out.print(n+ " ");
+    }
+    public static void main(String[] args){
+        int n = 5;
+        printInc(n);
+    }
+}
