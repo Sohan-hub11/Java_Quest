ec025582ffcb1ca0e61bd43d816128e05b38c59e
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-01 22:03:10 +0530

    Update Factorial.java

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,4 +1,18 @@
+//To find Factorial
+
 package Recursion;
 
 public class Factorial {
+    public static int fact(int n){
+        int f = 1;
+        if(n == 0){
+            return 1;
+        }
+        f = n * fact(n-1);
+        return f;
+    }
+    public static void main(String[] args){
+        int num = 5;
+        System.out.println(fact(num));
+    }
 }
