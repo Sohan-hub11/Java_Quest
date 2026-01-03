c61c9d24962bea56c59abae006560af23ced367e
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-03 14:11:41 +0530

    Update QuickSort.java

--- Divide & Conquer/QuickSort.java
+++ Divide & Conquer/QuickSort.java
@@ -1,4 +1,47 @@
+//Quick Sort
+
 package Divide_Conquer;
 
 public class QuickSort {
+    public static void printArray(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    public static void quickSort(int[] arr, int si, int ei){
+        //Base Case.
+        if(si >= ei){
+            return;
+        }
+
+        //Working.
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1); //left part.
+        quickSort(arr, pIdx+1, ei); //right part.
+    }
+    public static int partition(int[] arr, int si, int ei){
+        int i = si-1; //to make place for lesser element.
+        int pivot = arr[ei];
+
+        for(int j=si; j<ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+    public static void main(String[] args){
+        int[] arr = {6, 3, 9, 8, 2, 5};
+        quickSort(arr, 0, arr.length-1);
+        printArray(arr);
+    }
 }
