cea4ce9619956f82b739ecebf3f2ad471d669a01
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-11 04:17:28 +0530

    Add files via upload

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,42 @@
+//To Check it's a Valid Parenthesis or not.
+
+package Stack;
+import java.util.*;
+public class ValidParenthesis {
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<str.length()-1; i++){
+            char ch = str.charAt(i);
+
+            //Opening
+            if(ch == '(' || ch=='{' || ch=='['){
+                s.push(ch);
+            }
+
+            //Closing
+            else {
+                if(s.isEmpty()){
+                    return false;
+                }
+                if (s.peek() == '(' && ch == ')' ||
+                        s.peek() == '{' && ch == '}' ||
+                        s.peek() == '[' && ch == ']') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if(s.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[]args){
+        String str = "()";
+        System.out.println(isValid(str));;
+    }
+}
