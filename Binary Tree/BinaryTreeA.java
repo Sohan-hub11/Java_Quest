a2a78938c4121c4a0e51e85b84a50499351e1981
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-12 22:00:49 +0530

    Add files via upload

---
+++ Binary Tree/BinaryTreeA.java
@@ -0,0 +1,48 @@
+//PreOrder Traversal
+
+package Binary_Tree;
+
+public class BinaryTreeA {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int[] nodes){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            //Putting val of left & right subtree.
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args){
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+        preOrder(root);
+    }
+}
