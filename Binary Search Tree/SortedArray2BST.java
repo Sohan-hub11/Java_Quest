494df3238c576a95b67306d9d6eb82bfa8d99aa5
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-14 17:06:33 +0530

    Add files via upload

---
+++ Binary Search Tree/SortedArray2BST.java
@@ -0,0 +1,42 @@
+//Sorted Array to Balanced BST.
+
+package Binary_Search_Tree;
+
+public class SortedArray2BST {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static Node createBST(int[] arr, int st, int end){
+        if(st > end){
+            return null;
+        }
+
+        int mid = st + (end-st) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+
+        return root;
+    }
+    public static void main(String[] args){
+        int[] arr = {3, 5, 6, 8, 10, 11, 12};
+
+        Node root = createBST(arr, 0, arr.length-1);
+        inOrder(root);
+    }
+}
