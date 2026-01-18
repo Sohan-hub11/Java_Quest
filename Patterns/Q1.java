467295eb906fe6a08c9bc4680a14ed3ad1ad1b99
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-18 21:02:01 +0530

    Add files via upload

---
+++ Patterns/Q1.java
@@ -0,0 +1,26 @@
+/*
+    *
+   *
+  *
+ *
+*
+*/
+package Patterns;
+
+public class Q1 {
+    public static void main(String[] args){
+        int k = 4;
+        for(int i = 0; i<5; i++){
+            for(int j=0; j<5; j++){
+                if(j == k){
+                    System.out.print("*");
+                    k--;
+                    break;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
