c858beaffd9316dd84f863816fe5e5e1af4b209f
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-29 21:51:57 +0530

    Add files via upload

---
+++ Heaps/HeapPeek.java
@@ -0,0 +1,35 @@
+//Insertion in Heap
+
+package Heaps;
+
+import java.util.*;
+
+public class HeapPeek {
+    static class Heap{
+        ArrayList<Integer> list = new ArrayList<>();
+
+        public void add(int data){
+            list.add(data);
+            int childIdx = list.size()-1;
+            int parentIdx = (childIdx - 1)/2;
+
+            while (list.get(childIdx) < list.get(parentIdx)){
+                int temp = list.get(childIdx);
+                list.set(childIdx, list.get(parentIdx));
+                list.set(parentIdx, temp);
+            }
+        }
+        public int peek(){
+            return list.get(0);
+        }
+    }
+    public static void main(String[] args){
+        Heap h = new Heap();
+
+        h.add(2);
+        h.add(3);
+        h.add(4);
+        h.add(5);
+        h.add(10);
+    }
+}
