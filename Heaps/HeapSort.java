01e620f3a98bd8960b0e295fe85e29b008a5adf5
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-01-31 16:50:45 +0530

    Update HeapSort.java

--- Heaps/HeapSort.java
+++ Heaps/HeapSort.java
@@ -1,6 +1,61 @@
-//Heap Sort
+//Sorting in Ascending Order by Heap Sort
 
 package Heaps;
-import java.util.*;
+
 public class HeapSort {
+    public static void heapSort(int[] arr){
+        int n = arr.length;
+
+        //Step 1 -> maxHeap Creation
+        for(int i=n/2; i>=0; i--){
+            heapify(arr, i, n);
+        }
+
+        //Step 2 -> Largest-First element swap with last.
+        for(int i=n-1; i>0; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr,0, i);
+        }
+    }
+
+    public static void heapify(int[] arr, int i, int n){
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int maxIdx = i;
+
+        if(left < n && arr[left] > arr[maxIdx]){
+            maxIdx = left;
+        }
+
+        if(right < n && arr[right] > arr[maxIdx]){
+            maxIdx = right;
+        }
+
+        if(maxIdx != i){
+            int temp = arr[maxIdx];
+            arr[maxIdx] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, maxIdx, n);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1, 2, 4, 5, 3};
+
+        printArray(arr);
+
+        heapSort(arr);
+        printArray(arr);
+    }
 }
