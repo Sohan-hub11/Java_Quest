6d87956e07f1831aef49fba6dfcb25e2a2ee283c
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-02-10 15:59:31 +0530

    Update Permutation.java

--- Backtracking/Permutation.java
+++ Backtracking/Permutation.java
@@ -1,4 +1,24 @@
+//Find and Print all Permutation of the String.
+
 package Backtracking;
 
 public class Permutation {
+    public static void findPermutation(String str, String ans){
+        //Base Case.
+        if(str.length() == 0){
+            System.out.println(ans);
+        }
+        //Recursion.
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            String newStr = str.substring(0, i) + str.substring(i+1);
+
+            findPermutation(newStr, ans+ch);
+        }
+    }
+    public static void main(String[] args){
+        String str = "abc";
+        findPermutation(str, "");
+    }
 }
