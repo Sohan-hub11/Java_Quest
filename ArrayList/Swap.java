c5e76fb15ba9dfa4468005661b883d60b3547210
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-02-26 11:40:38 +0530

    Add files via upload

---
+++ ArrayList/Swap.java
@@ -0,0 +1,22 @@
+package ArrayList;
+import java.util.*;
+public class Swap {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
+
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        list.add(11);
+        list.add(4);
+        list.add(6);
+        System.out.println(list);
+
+        swap(list, 1, 4);
+        System.out.println(list);
+    }
+}
