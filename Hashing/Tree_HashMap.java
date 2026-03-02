d2324b9de69f02ee489982aed92d7bfe5ef71712
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-03-03 02:47:46 +0530

    Add files via upload

---
+++ Hashing/Tree_HashMap.java
@@ -0,0 +1,28 @@
+//Tree Hash Map.
+
+package Hashing;
+
+import java.util.*;
+
+public class Tree_HashMap {
+    public static void main(String[] args){
+        TreeMap<String, Integer> thm = new TreeMap<>();
+        thm.put("Tea", 10);
+        thm.put("Samosa", 15);
+        thm.put("Pizza", 150);
+        thm.put("Coffee", 15);
+        thm.put("Burger", 120);
+        thm.put("Biriyani", 250);
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Tea", 10);
+        map.put("Samosa", 15);
+        map.put("Pizza", 150);
+        map.put("Coffee", 15);
+        map.put("Burger", 120);
+        map.put("Biriyani", 250);
+
+        System.out.println("HashMap -> " + map);
+        System.out.println("Linked HashMap -> " + thm);
+    }
+}
