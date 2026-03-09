575051fa8d8ec84eec43d2fbafc83561cc34cdc9
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-03-09 21:16:55 +0530

    Add files via upload

---
+++ Hashing/SubArraySum.java
@@ -0,0 +1,25 @@
+//Sub Array sum equals to k
+
+package Hashing;
+import java.util.*;
+public class SubArraySum {
+    public static void main(String[] args){
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+
+        int sum = 0;
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); //to check if a single index value equals to k.
+        for(int j=0; j< arr.length; j++){
+            sum += arr[j];
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) +1);
+        }
+
+        System.out.println(ans);
+    }
+}
