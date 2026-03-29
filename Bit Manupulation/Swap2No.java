6add6df139a81021d09ee26e21fda106839c1213
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2026-03-29 16:45:20 +0530

    Add XOR-based swap implementation in Swap2No.java
    Implemented swapping of two numbers using XOR without a third variable.

--- Bit Manupulation/Swap2No.java
+++ Bit Manupulation/Swap2No.java
@@ -5,4 +5,16 @@
 package BitManipulation;
 
 public class Swap2No {
+    public static void main (String[] args){
+        int a = 4;
+        int b = 5;
+        System.out.println("Before Swapping --");
+        System.out.println("a : " + a + " " + "b : " + b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After Swapping --");
+        System.out.println("a : " + a + " " + "b : " + b);
+    }
 }
